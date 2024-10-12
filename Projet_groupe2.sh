3ef6ebe6507f4d832ae9b1b2a98cad292558b600
MaximeMiage <maxime.carmouze@toulouse.miage.fr>
2024-10-12 11:57:50 +0200

    Create Projet_groupe2.sh

---
+++ Projet_groupe2.sh
@@ -0,0 +1,23 @@
+#Projet configuration d'un poste de travail
+
+#!/bin/bash
+
+#Vérifier qu'on entre bien un seul argument
+
+if [[ $# -ne 1 ]]; 
+then
+ echo "Veuillez saisir une seule ville ! "
+fi
+
+#On crée le fichier pour les tempérture et le temporaire
+tempo="meteo_tempo.txt"
+meteo="meteo.txt"
+#On récupère la ville
+ville="$1"
+
+#On récupère les données dans wttr.in avec curl et on les envoie dans le tempo
+curl -s "wttr.in/$VILLE?format=%C+%t+%f+%p" > "$tempo"
+
+
+
+
